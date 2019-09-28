51048f3e67fc20b938816e318039ab21dd2c0cd6
JoelJonas93 <jonasjoel@ymail.com>
2019-09-27 21:18:03 -0300

    Tabela proprietarios 1.2

--- ImovelCerto/database/migrations/2019_09_27_203927_create_proprietarios_table.php
+++ ImovelCerto/database/migrations/2019_09_27_203927_create_proprietarios_table.php
@@ -15,15 +15,17 @@ class CreateProprietariosTable extends Migration
     {
         Schema::create('proprietarios', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nome',50);
-            $table->string('cpf',14);
-            $table->string('email',50);
-            $table->date('data_nascimento');
-            $table->string('fone_cel',15);
-            $table->string('fone_res',14);
-            $table->string('endereco',50);
-            $table->string('cep',10);
-            $table->string('uf');
+            $table->string('nome', 50);
+            $table->string('cpf', 14)->unique();
+            $table->string('fone_res', 14)->nullable();
+            $table->string('fone_cel', 15);
+            $table->string('endereco', 50);
+            $table->string('bairro', 30);
+            $table->string('cep', 10);
+            $table->string('cidade', 50);
+            $table->char('uf', 2);
+            $table->string('email', 50);
+            $table->date('dt_nasc');
             $table->timestamps();
         });
     }
